2a80007c5ffc83587c26fe6853e66ad7a626f5af
i-deal <ianmdeal@gmail.com>
2024-02-22 13:13:52 -0500

    include fmnist in training

--- Training.py
+++ Training.py
@@ -18,21 +18,21 @@ bs=100
 
 # trainging datasets, the return loaders flag is False so the datasets can be concated in the dataloader
 emnist_dataset, emnist_skip, emnist_test_dataset = dataset_builder('emnist', bs, None, True, {'left':list(range(0,5)),'right':list(range(5,10))}, False)
-#fmnist_dataset, fmnist_skip, fmnist_test_dataset = dataset_builder('fashion_mnist', bs, None, True, None, False)
+fmnist_dataset, fmnist_skip, fmnist_test_dataset = dataset_builder('fashion_mnist', bs, None, True, None, False)
 mnist_dataset, mnist_skip, mnist_test_dataset = dataset_builder('mnist', bs, None, True, {'left':list(range(0,5)),'right':list(range(5,10))}, False)
 
 
 #concat datasets and init dataloaders
-train_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_dataset, mnist_dataset]), batch_size=bs, shuffle=True,  drop_last= True)
-test_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_test_dataset, mnist_dataset]), batch_size=bs, shuffle=True, drop_last=True)
-emnist_skip = torch.utils.data.DataLoader(dataset=emnist_skip, batch_size=bs, shuffle=True,  drop_last= True)
-mnist_skip = torch.utils.data.DataLoader(dataset=mnist_skip, batch_size=bs, shuffle=True,  drop_last= True)
+train_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_dataset, fmnist_dataset, mnist_dataset]), batch_size=bs, shuffle=True,  drop_last= True)
+test_loader_noSkip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_test_dataset, fmnist_test_dataset, mnist_test_dataset]), batch_size=bs, shuffle=True, drop_last=True)
+emnist_skip = torch.utils.data.DataLoader(dataset=ConcatDataset([emnist_dataset, mnist_dataset]), batch_size=bs, shuffle=True,  drop_last= True)
+fmnist_skip = torch.utils.data.DataLoader(dataset=fmnist_skip, batch_size=bs, shuffle=True,  drop_last= True)
 
 
 loss_dict = {'retinal_train':[], 'retinal_test':[], 'cropped_train':[], 'cropped_test':[]}
 
 for epoch in range(1, 301):
-    loss_lst = train(epoch, train_loader_noSkip, emnist_skip, mnist_skip, test_loader_noSkip, True)
+    loss_lst = train(epoch, train_loader_noSkip, emnist_skip, fmnist_skip, test_loader_noSkip, True)
     
     # save error quantities
     loss_dict['retinal_train'] += [loss_lst[0]]
