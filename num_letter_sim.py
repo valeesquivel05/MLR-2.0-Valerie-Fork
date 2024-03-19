d22671f503b00bb6777a1abeb8bcf058cba2d89a
i-deal <ianmdeal@gmail.com>
2024-03-19 00:06:19 -0400

    update simulation

--- num_letter_sim.py
+++ num_letter_sim.py
@@ -15,32 +15,32 @@ clf_shapeS=load('classifier_output/ss.joblib')
 
 vals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+# char combinations: [1,3: B], [L,1: U], []
+
 vae.eval()
 with torch.no_grad():
-    num1 = 1 # first number
-    num2 = 3 # second number
+    num1 = 21 #1 # first char
+    num2 = 1 #3 # second char
     #colors = torch.randint(low=0, high=10, size=(2,))
 
     # build one hot vectors to be passed to the label networks
-    num_labels = F.one_hot(torch.tensor([num1, num2]).cuda(), num_classes=s_classes).float().cuda()
+    num_labels = F.one_hot(torch.tensor([num1, num2]).cuda(), num_classes=s_classes).float().cuda() # shape
+    loc_labels = torch.zeros((2,100)).cuda()
+    loc_labels[0][0], loc_labels[1][8] = 1, 1 # location
     #col_labels = F.one_hot(torch.tensor([num1, num2]).cuda(), num_classes=10).float().cuda()
 
     # generate shape latents from the labels n = noise
-    z_shape_labels = vae_shape_labels(num_labels, n=1)
+    z_shape_labels = vae_shape_labels(num_labels, n = 1)
+
+    # location latent from the location vector
+    z_location = vae.location_encoder(loc_labels)
 
     # pass latents from label network through encoder
-    recon_shape = vae.decoder_shape(z_shape_labels, 0, 0)
+    recon_retinal = vae.decoder_retinal(z_shape_labels, 0, z_location, None, 'shape')
     # clamp shape recons to form one image of the combined numbers
-    img1 = recon_shape[0]
-    print(img1.size())
-    img1[:,0:14,:] = img1[:,8:22,:]
-    #img1[1:3][15:20][0:27] = torch.zeros(3,5,28)
-    print(img1.max())
-    img2 = recon_shape[1]
+    img1 = recon_retinal[0,:,:,6:34]
+    img2 = recon_retinal[1,:,:,6:34]
     comb_img = torch.clamp(img1 + img2, 0, 1)
-    #comb_img = torch.mean(img1+img2)
-    
-    print(comb_img.max())
     comb_img = comb_img.view(1,3,28,28)
 
     
@@ -51,7 +51,7 @@ with torch.no_grad():
 
     recon_shape = vae.decoder_shape(z_shape, 0, 0)
     utils.save_image(comb_img,'1_3_sim.png')
-    utils.save_image(recon_shape,'2_3_sim.png')
+    utils.save_image(recon_shape,'1_3_sim_recon.png')
     utils.save_image(img1,'img1.png')
     utils.save_image(img2,'img2.png')
     print(pred_ss)
